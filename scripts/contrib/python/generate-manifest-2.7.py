12ae2ca4e6238226cf7373f54018fe8492710907
Maciej Borzecki <maciej.borzecki@open-rnd.pl>
2014-12-18 14:10:42 +0100

    python-2.7-manifest.inc: add stdlib argparse module
    Adding argparse module from Python's standard library. The module has replaced
    previous python-argparse package that was build using source that are not part
    of Python 2.7 release.
    
    Signed-off-by: Maciej Borzecki <maciej.borzecki@open-rnd.pl>
    Signed-off-by: Maciek Borzecki <maciek.borzecki@gmail.com>

--- scripts/contrib/python/generate-manifest-2.7.py
+++ scripts/contrib/python/generate-manifest-2.7.py
@@ -170,7 +170,7 @@ if __name__ == "__main__":
     "warnings.* site.* stat.* " +
     "UserDict.* UserList.* UserString.* " +
     "lib-dynload/binascii.so lib-dynload/_struct.so lib-dynload/time.so " +
-    "lib-dynload/xreadlines.so types.* platform.* ${bindir}/python* "  + 
+    "lib-dynload/xreadlines.so types.* platform.* ${bindir}/python* "  +
     "_weakrefset.* sysconfig.* config/Makefile " +
     "${includedir}/python${PYTHON_MAJMIN}/pyconfig*.h " +
     "${libdir}/python${PYTHON_MAJMIN}/sitecustomize.py ")
@@ -387,5 +387,8 @@ if __name__ == "__main__":
 
     m.addPackage( "${PN}-mailbox", "Python mailbox format support", "${PN}-core ${PN}-mime",
     "mailbox.*" )
+
+    m.addPackage( "${PN}-argparse", "Python command line argument parser", "${PN}-core ${PN}-codecs ${PN}-textutils",
+    "argparse.*" )
 
     m.make()
